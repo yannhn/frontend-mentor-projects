7f2607c89321ebdb308e9f4e1933b3812e5279eb
Yannik Hangst <yanso1@posteo.de>
2023-04-17 00:01:13 +0200

    setup components

---
+++ 01_news-homepage/src/components/MainPreview.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainPreview = () => {
+  return <div>MainPreview</div>;
+};
+
+export default MainPreview;
---
+++ 01_news-homepage/src/components/SmallPreview.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SmallPreview = () => {
+  return <div>SmallPreview</div>;
+};
+
+export default SmallPreview;
